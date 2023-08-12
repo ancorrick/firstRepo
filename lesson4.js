bb400b5c38dedf28609d90d07a38aa08cb2295f3
Andranik Yeritsyan <andranikyeritysan@gmail.com>
2023-08-12 17:35:13 +0400

    newnew

--- lesson4.js
+++ lesson4.js
@@ -1,7 +1,18 @@
-console.log("hello");
+const matrixArr = [
+    [1, 2, 3],
+    [100, 200, 300, 400],
+    [4, 5, 6, 7, 8],
+    // [500, 600],
+]
 
-console.log("asdkjhaskjhddhjkasakhjs");
+let maxNum
 
-console.log("sadlkasllk;adskadsk;l");
+for (let i = 0; i < matrixArr.length; i++) {
+    for(let j = 0 ; j < matrixArr.length; j++){
+        maxNum = Math.max.apply(null, matrixArr[i][])
+    }
+}
 
-console.log("asdkjsajdkkjhasdhkjsadkhjasdhkjdsakhjsadhkj");
+
+
+console.log(maxNum)
